71c2d668443042c6d1c63375e8fedc2c7d085b8a
VeroPapp <papp.veronica@gmail.com>
2024-06-18 20:28:53 +0200

    registro de usuarios

--- app.js
+++ app.js
@@ -28,7 +28,47 @@ app.use(session({
     saveUninitialized: true,
 }));
 
+// 8- invocamos al modulo de conexion de la bd
+const connection = require('./database/db');
+
+// 9- establecemos las rutas
+app.get('/', (req, res)=>{
+    res.render('index', {msg: 'msj de prueba'})
+});
+
+app.get('/login', (req, res)=>{
+    res.render('login')
+});
+
+app.get('/register', (req, res)=>{
+    res.render('register')
+});
+
+// 10- registracion
+app.post('/register', async (req, res)=> {
+    const user = req.body.user;
+    const name = req.body.name;
+    const rol = req.body.rol;
+    const pass = req.body.pass;
+    let passwordHaash = await bcryptjs.hash(pass, 8);
+    connection.query('INSERT INTO user SET ?', {user:user,name:name, rol:rol, pass:passwordHaash}, async(error, results)=> {
+        if(error){
+            console.log(error);
+        } else {
+            res.render('register',{
+                alert: true,
+                alertTitle: "Registration",
+                alertMessage: "Successful Registration",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer: 1500,
+                ruta: ''
+            })
+        }
+    })
+})
 
 app.listen(3000, (req, res)=>{
     console.log('Servidor corriendo en puerto 3000');
 })
+ 
