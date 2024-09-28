692ed8eb65a3de14fa171128ce6745f780dc0940
VeroPapp <papp.veronica@gmail.com>
2024-09-28 11:32:43 -0300

    conecta a la bd

--- app.js
+++ app.js
@@ -1,74 +1,151 @@
-// 1- invocamos express
+// 1 - Invocamos a Express
 const express = require('express');
 const app = express();
 
-// 2- seteamos urlencoded para capturar los datos del formulario
+//2 - Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
 app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+app.use(express.json());//además le decimos a express que vamos a usar json
 
-// 3- invocamos dotenv
+//3- Invocamos a dotenv
 const dotenv = require('dotenv');
-dotenv.config({path:'./env/.env'});
+dotenv.config({ path: './env/.env'});
 
-// 4- seteamos el directorio public
-app.use('/resources', express.static('public'));
+//4 -seteamos el directorio de assets
+app.use('/resources',express.static('public'));
 app.use('/resources', express.static(__dirname + '/public'));
 
-// 5- establecemos el motor de plantillas
-app.set('view engine', 'ejs');
+//5 - Establecemos el motor de plantillas
+app.set('view engine','ejs');
 
-// 6- modulo para el hashing de psw - bcryptjs
-const bcryptjs = require('bcryptjs');
+//6 -Invocamos a bcrypt
+const bcrypt = require('bcryptjs');
 
-// 7- variables de sesion
+//7- variables de session
 const session = require('express-session');
 app.use(session({
-    secret:'secret',
-    resave: true,
-    saveUninitialized: true,
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
 }));
 
-// 8- invocamos al modulo de conexion de la bd
+
+// 8 - Invocamos a la conexion de la DB
 const connection = require('./database/db');
 
-// 9- establecemos las rutas
-app.get('/', (req, res)=>{
-    res.render('index', {msg: 'msj de prueba'})
-});
+//9 - establecemos las rutas
+	app.get('/login',(req, res)=>{
+		res.render('login');
+	})
 
-app.get('/login', (req, res)=>{
-    res.render('login')
-});
+	app.get('/register',(req, res)=>{
+		res.render('register');
+	})
 
-app.get('/register', (req, res)=>{
-    res.render('register')
-});
-
-// 10- registracion
-app.post('/register', async (req, res)=> {
-    const user = req.body.user;
-    const name = req.body.name;
-    const rol = req.body.rol;
-    const pass = req.body.pass;
-    let passwordHaash = await bcryptjs.hash(pass, 8);
-    connection.query('INSERT INTO user SET ?', {user:user,name:name, rol:rol, pass:passwordHaash}, async(error, results)=> {
+//10 - Método para la REGISTRACIÓN
+app.post('/register', async (req, res)=>{
+	const name = req.body.name;
+    const mail = req.body.mail;
+	const pass = req.body.pass;
+	let passwordHash = await bcrypt.hash(pass, 8);
+    connection.query('INSERT INTO user SET ?',{name:name, mail:mail, pass:passwordHash}, async (error, results)=>{
         if(error){
             console.log(error);
-        } else {
-            res.render('register',{
-                alert: true,
-                alertTitle: "Registration",
-                alertMessage: "Successful Registration",
-                alertIcon: 'success',
-                showConfirmButton: false,
-                timer: 1500,
-                ruta: ''
-            })
+        }else{            
+			res.render('register', {
+				alert: true,
+				alertTitle: "Registration",
+				alertMessage: "¡Successful Registration!",
+				alertIcon:'success',
+				showConfirmButton: false,
+				timer: 1500,
+				ruta: ''
+			});      
         }
-    })
+	});
 })
 
+//11 - Metodo para la autenticacion
+app.post('/auth', async (req, res)=> {
+	const name = req.body.name;
+	const pass = req.body.pass;    
+    //let passwordHash = await bcrypt.hash(pass, 8);
+	
+	if (name && pass) {
+		connection.query('SELECT * FROM user WHERE name = ?', [name], async (error, results, fields)=> {
+			//console.log(results);
+
+			/*if (results.length > 0) {
+				const passwordMatches = await bcrypt.compare(pass, results[0].pass);
+				console.log("Contraseña ingresada: ", pass);
+				console.log("Hash en la base de datos: ", results[0].pass);
+				console.log("Coinciden las contraseñas: ", passwordMatches);*/
+
+			if( results.length == 0 || !(await bcrypt.compare(pass, results[0].pass)) ) {    
+				res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "USUARIO y/o PASSWORD incorrectas",
+                        alertIcon:'error',
+                        showConfirmButton: true,
+                        timer: false,
+                        ruta: 'login'    
+                    });
+						
+			} else {         
+				//creamos una var de session y le asignamos true si INICIO SESSION       
+				req.session.loggedin = true;                
+				req.session.name = results[0].name;
+				res.render('login', {
+					alert: true,
+					alertTitle: "Conexión exitosa",
+					alertMessage: "¡LOGIN CORRECTO!",
+					alertIcon:'success',
+					showConfirmButton: false,
+					timer: 1500,
+					ruta: ''
+				});        			
+			}			
+			res.end();
+		});
+	} else {	
+		res.send('Please enter user and Password!');
+		res.end();
+	}
+});
+
+//12 - Método para controlar que está auth en todas las páginas
+app.get('/', (req, res)=> {
+	if (req.session.loggedin) {
+		res.render('index',{
+			login: true,
+			name: req.session.name			
+		});		
+	} else {
+		res.render('index',{
+			login:false,
+			name:'Debe iniciar sesión',			
+		});				
+	}
+	res.end();
+});
+
+
+//función para limpiar la caché luego del logout
+app.use(function(req, res, next) {
+    if (!req.user)
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
+
+ //Logout
+//Destruye la sesión.
+app.get('/logout', function (req, res) {
+	req.session.destroy(() => {
+	  res.redirect('/') // siempre se ejecutará después de que se destruya la sesión
+	})
+});
+
+
 app.listen(3000, (req, res)=>{
-    console.log('Servidor corriendo en puerto 3000');
-})
- 
+    console.log('SERVER RUNNING IN http://localhost:3000');
+});  
--- database/db.js
+++ database/db.js
@@ -1,18 +1,19 @@
+//2 - Invocamos a MySQL y realizamos la conexion
 const mysql = require('mysql');
-
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    //Con variables de entorno
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE
 });
 
 connection.connect((error)=>{
-    if(error){
-        console.log('El error de conexion es : ' +error);
-        return;
+    if (error) {
+      console.error('El error de conexión es: ' + error);
+      return;
     }
-    console.log('Conectado a la base de datos!');
-});
+    console.log('¡Conectado a la Base de Datos!');
+  });
 
-module.exports = connection;
+  module.exports = connection;
